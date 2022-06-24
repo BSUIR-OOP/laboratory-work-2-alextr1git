d696092bc08eacb8b690b48adef3f12ebe424990
Alexander <dasprooa3@gmail.com>
2022-06-25 01:23:09 +0300

    Final

---
+++ Builder/FigureBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrawFiguresLab.FiguresLR1;
+using System.Windows;
+using System.Reflection;
+
+namespace DrawFiguresLab.Builder
+{
+    public static class FigureBuilder
+    {
+        public static ParentFigure Build(Type TypeofFigure, Point? point1, Point? point2)
+        {
+            ConstructorInfo fbuilder = TypeofFigure.GetConstructor(new[] { typeof(Point), typeof(Point) }); //get correct contructor
+
+            return (ParentFigure)fbuilder.Invoke(new object[] { point1, point2 }); //create instance
+
+        }
+
+    }
+}
---
+++ Models/Figures/ParentFigure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace DrawFiguresLab.FiguresLR1
+{
+    public abstract class ParentFigure
+    {
+        public abstract void drawCanvas(Canvas canvas);
+
+        public static System.Windows.Shapes.Line drawLine(int x1, int x2, int y1, int y2)
+        {
+            var NewLine = new System.Windows.Shapes.Line();
+            NewLine.Stroke = System.Windows.Media.Brushes.Black;
+
+            NewLine.X1 = x1;
+            NewLine.X2 = x2;
+
+            NewLine.Y1 = y1;
+            NewLine.Y2 = y2;
+
+            NewLine.HorizontalAlignment = HorizontalAlignment.Left;
+            NewLine.VerticalAlignment = VerticalAlignment.Center;
+
+            NewLine.StrokeThickness = 3;
+
+            return NewLine;
+        }
+
+    }
+}
